b82a34fb5e9bdf7ab946221246cc0f36001182b4
RupinVijan <rupinvijan@gmail.com>
2022-08-28 16:06:50 +0530

    code refined

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,12 +8,12 @@ const bcrypt = require('bcryptjs')
 const jwt_token = process.env.JWT_SECRET;
 
 const hashPassword = async(password) => {
-        const salt = await  bcrypt.genSaltSync(process.env.GEN_SALT);
-        const hashedPassword = bcrypt.hashSync(password, salt);
+        const salt = await  bcrypt.genSaltSync(10);
+        const hashedPassword = await bcrypt.hashSync(password, salt);
         return (hashedPassword);
 }
 const jwtCode = async(id) => {
-        const userToken = jwt.sign({ id }, jwt_token);
+        const userToken =await jwt.sign({ id }, jwt_token);
         return (userToken);
 }
 
@@ -24,21 +24,23 @@ router.post('/signup' ,async(req,res) => {
     try {
       let user = await userModel.findOne({email:req.body.email});
       if(user) return res.status(400).send({status:false, msg:'User Alread Exist!'})
-      
-      user = await userModel.create({...req.body , password:hashPassword(req.body.password)})
-      const userToken = jwtCode(user.id);
+      let pass = await hashPassword(req.body.password)
+      user = await userModel.create({...req.body , password:pass})
+      const userToken = await jwtCode(user.id);
       return res.status(200).send({status:true,msg:'User Created Successfully!' ,user , userToken})
     } catch (error) {
+       
       res.status(500).send({status:false,msg:'Internal Server Error!'})
     }
   })
   router.post('/login' ,async(req,res) => {
     try {
       let user = await userModel.findOne({email:req.body.email});
-      if(!user || !bcrypt.compareSync(req.body.password, userToFind.password)) return res.status(404).send({status:false, msg:'Invalid Credentials!'})
-      const userToken = jwtCode(user.id);
+      if(!user || !bcrypt.compareSync(req.body.password, user.password)) return res.status(404).send({status:false, msg:'Invalid Credentials!'})
+      const userToken = await jwtCode(user.id);
       return res.status(200).send({status:true,msg:'Logged in Successfully!' , user , userToken})
     } catch (error) {
+        console.log(error)
       res.status(500).send({status:false,msg:'Internal Server Error!'})
     }
   })
